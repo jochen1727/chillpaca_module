4005d714b0a291188c0d6708a5e00a782b076729
chillpaca <31731485+jochen1727@users.noreply.github.com>
2024-03-09 18:42:48 +0100

    1

--- chillpaca_module.psm1
+++ chillpaca_module.psm1
@@ -1,5 +1,5 @@
 #module chillpaca administration
 . $PSScriptRoot\chillpaca_ad_createuser.ps1
-#. $PSScriptRoot\chillpaca_apps_installchoco.ps1
+. $PSScriptRoot\chillpaca_apps_installchoco.ps1
 #. $PSScriptRoot\chillpaca_apps_installwinget.ps1
-#. $PSScriptRoot\chillpaca_rds_cleansession.ps1
+. $PSScriptRoot\chillpaca_rds_cleansession.ps1
